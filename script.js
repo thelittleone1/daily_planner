243e222db8d7ab110a18b7af126735eb4da26333
Urooj Qureshi <qurooj0226@gmail.com>
2022-03-26 14:10:45 -0700

    New Commits

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Function that displays current time to top of page
 function displayTime() {
    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+   for(let box in dataValues){
+    timeColorDisplay(dataValues[box])
+  }
 }
-setInterval(displayTime, 1000);
+setInterval(displayTime, 3000);
 
 // Display Current Time on the Page
 var currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -11,18 +14,14 @@ console.log(currentDay);
 // Grabbing the input values from User
 let dataEntry = $(".col-10 border");
 
-// Loop through all the elements
-//for(let i = 0; i < dataEntry.length; i++) {}
-
 // Display colors based on time of activities 
-function timeColorDisplay() {
+function timeColorDisplay(time) {
   // Gather all the elements that contain data-hour attribute
-  let timeValues = ($("data-hour"));
-  console.log(timeValues);
+  let timeValues = Number(time.attr('data-hour')) - currentDay.hour();
 
-  if (timeValues < currentDay) {
+  if (timeValues < 0) {
     time.addClass("past");
-} else if (timeValues > currentDay) {
+} else if (timeValues > 0) {
     time.addClass("future");
 } else {
     time.addClass("present");
@@ -30,15 +29,15 @@ function timeColorDisplay() {
 
 // Creating an object to hold the value of events
 let dataValues = {
-  h9: $("#h9").val(),
-  h10: $("#h10").val(),
-  h11: $("#h11").val(),
-  h12: $("#h12").val(),
-  h13: $("#h13").val(),
-  h14: $("#h14").val(),
-  h15: $("#h15").val(),
-  h16: $("#h16").val(),
-  h17: $("#h17").val()
+  h9: $("#h9"),
+  h10: $("#h10"),
+  h11: $("#h11"),
+  h12: $("#h12"),
+  h13: $("#h13"),
+  h14: $("#h14"),
+  h15: $("#h15"),
+  h16: $("#h16"),
+  h17: $("#h17")
 }
 
 // Creating a function to set the local data
@@ -52,6 +51,7 @@ console.log(storedData);
 
 // The save button save the contents of the page
 $("saveButton").on("click", saveData);
+timeColorDisplay();
 
 //function timeColorDisplay() {
   // var hourTime = parseInt.attr("id");
